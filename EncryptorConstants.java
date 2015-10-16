978f4b4f2539548e486c0ee91df96ba706e907af
Ivan <zvezdo4eet@gmail.com>
2015-10-16 13:33:44 +0100

    Update EncryptorConstants.java

--- EncryptorConstants.java
+++ EncryptorConstants.java
@@ -7,8 +7,8 @@ public interface EncryptorConstants {
     int BTN_WIDTH = 90;
     int BTN_HEIGHT = 35;
 
-    int MATRIX_SIZE = 4;
-    int NUMBER_OF_LETTERS = 65536;
+    int MATRIX_SIZE = 4;            // 2 by 2 Matrix
+    int NUMBER_OF_LETTERS = 65536;  // char represents 2 byte in Java -> hence  2 ^ 16 = 65636
 //    char[] ALPHABET = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 //            ' ', 13, 10, '!', ',', '.'};
 
